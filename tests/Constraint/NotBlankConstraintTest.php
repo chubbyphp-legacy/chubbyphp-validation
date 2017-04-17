c207fc43625de1a5fe24eca00297e6305c308987
Dominik Zogg <dominik.zogg@gmail.com>
2017-04-17 13:06:54 +0200

    add not blank constraint test

---
+++ tests/Constraint/NotBlankConstraintTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Chubbyphp\Tests\Validation\Constraint;
+
+use Chubbyphp\Validation\Constraint\NotBlankConstraint;
+use Chubbyphp\Validation\Error\Error;
+
+/**
+ * @covers \Chubbyphp\Validation\Constraint\NotBlankConstraint
+ */
+class NotBlankConstraintTest extends \PHPUnit_Framework_TestCase
+{
+    public function testWithNullValue()
+    {
+        $constraint = new NotBlankConstraint();
+
+        self::assertEquals([], $constraint->validate('notblank', null));
+    }
+
+    public function testWithNotBlank()
+    {
+        $constraint = new NotBlankConstraint();
+
+        self::assertEquals([], $constraint->validate('notblank', 'notblank'));
+    }
+
+    public function testWithBlankString()
+    {
+        $constraint = new NotBlankConstraint();
+
+        $error = new Error('notblank', 'constraint.notblank');
+
+        self::assertEquals([$error], $constraint->validate('notblank', ''));
+    }
+
+    public function testWithBlankArray()
+    {
+        $constraint = new NotBlankConstraint();
+
+        $error = new Error('notblank', 'constraint.notblank');
+
+        self::assertEquals([$error], $constraint->validate('notblank', []));
+    }
+}
