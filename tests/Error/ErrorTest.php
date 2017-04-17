16cc458d48aae5a92e68e895dc1ff495a61f667a
Dominik Zogg <dominik.zogg@gmail.com>
2017-04-17 14:56:06 +0200

    add error test

---
+++ tests/Error/ErrorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Chubbyphp\Tests\Validation\Error;
+
+use Chubbyphp\Validation\Error\Error;
+
+/**
+ * @covers \Chubbyphp\Validation\Error\Error
+ */
+class ErrorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testWithoutArguments()
+    {
+        $path = 'path';
+        $key = 'key';
+
+        $error = new Error($path, $key);
+
+        self::assertSame($path, $error->getPath());
+        self::assertSame($key, $error->getKey());
+        self::assertSame([], $error->getArguments());
+    }
+
+    public function testWithArguments()
+    {
+        $path = 'path';
+        $key = 'key';
+        $arguments = ['input' => 'input'];
+
+        $error = new Error($path, $key, $arguments);
+
+        self::assertSame($path, $error->getPath());
+        self::assertSame($key, $error->getKey());
+        self::assertSame($arguments, $error->getArguments());
+    }
+}
