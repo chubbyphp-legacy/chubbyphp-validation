d205fd47e2727905d372afd6b509f18c84c8c320
Dominik Zogg <dominik.zogg@gmail.com>
2017-04-17 13:17:43 +0200

    add numeric constraint test

---
+++ tests/Constraint/NumericConstraintTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Chubbyphp\Tests\Validation\Constraint;
+
+use Chubbyphp\Validation\Constraint\NumericConstraint;
+use Chubbyphp\Validation\Error\Error;
+
+/**
+ * @covers \Chubbyphp\Validation\Constraint\NumericConstraint
+ */
+class NumericConstraintTest extends \PHPUnit_Framework_TestCase
+{
+    public function testWithNullValue()
+    {
+        $constraint = new NumericConstraint();
+
+        self::assertEquals([], $constraint->validate('numeric', null));
+    }
+
+    public function testWithInteger()
+    {
+        $constraint = new NumericConstraint();
+
+        self::assertEquals([], $constraint->validate('numeric', 1));
+    }
+
+    public function testWithFloat()
+    {
+        $constraint = new NumericConstraint();
+
+        self::assertEquals([], $constraint->validate('numeric', 1.1));
+    }
+
+    public function testWithString()
+    {
+        $constraint = new NumericConstraint();
+
+        self::assertEquals([], $constraint->validate('numeric', '1.1'));
+    }
+
+    public function testWithoutNumeric()
+    {
+        $constraint = new NumericConstraint();
+
+        $error = new Error('numeric', 'constraint.numeric', ['value' => 'test']);
+
+        self::assertEquals([$error], $constraint->validate('numeric', 'test'));
+    }
+}
