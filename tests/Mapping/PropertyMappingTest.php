82ec737618e7960d5cb20f88f15b269be0879347
Dominik Zogg <dominik.zogg@gmail.com>
2017-04-17 23:00:14 +0200

    add property mapping class

---
+++ tests/Mapping/PropertyMappingTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Chubbyphp\Tests\Validation\Mapping;
+
+use Chubbyphp\Validation\Constraint\ConstraintInterface;
+use Chubbyphp\Validation\Mapping\PropertyMapping;
+
+/**
+ * @covers \Chubbyphp\Validation\Mapping\PropertyMapping
+ */
+class PropertyMappingTest extends \PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $name = 'property1';
+        $constraints = [
+            $this->getConstraint(),
+            $this->getConstraint(),
+        ];
+
+        $propertyMapping = new PropertyMapping($name, $constraints);
+
+        self::assertSame($name, $propertyMapping->getName());
+        self::assertSame($constraints, $propertyMapping->getConstraints());
+    }
+
+    /**
+     * @return ConstraintInterface
+     */
+    private function getConstraint(): ConstraintInterface
+    {
+        return $this->getMockBuilder(ConstraintInterface::class)->getMockForAbstractClass();
+    }
+}
